f06350376869e220d5313c212d685b072a3e8ee6
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2022-11-19 14:11:56 -0500

    added session settings for deployment

--- app.py
+++ app.py
@@ -20,6 +20,13 @@ FRONTEND_URL = os.environ.get('FRONTEND_URL')
 # Initialize instance of the Flask class 
 app = Flask(__name__)
 # ================================================================
+# session settings for deployment 
+app.config.update(
+    SESSION_COOKIE_SECURE=True,
+    SESSION_COOKIE_HTTPONLY=True,
+    SESSION_COOKIE_SAMESITE='None',
+)
+# ================================================================
 login_manager = LoginManager()
 login_manager.init_app(app) # set up session on the app
 app.secret_key = os.environ.get('APP_SECRET')
